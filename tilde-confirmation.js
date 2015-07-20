3daec602a91f8a5775446b8eb4ad08d5ffde970e
Kevan Atkins <kevan.atkins@gmail.com>
2015-07-21 03:16:33 +1000

    Establishing basic HTML template.

--- tilde-confirmation.js
+++ tilde-confirmation.js
@@ -45,23 +45,7 @@ function SendConfirmationMail(e) {
     // This is the body of the auto-reply. You can tart this up with
     // any HTML with tilde branding and what not if you like. Or
     // pester me about it.
-    message = "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'> \
-              <html xmlns='http://www.w3.org/1999/xhtml'> \
-               <head> \
-                <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' /> \
-                <title>Tilde New Music and Sound Art</title> \
-                <meta name='viewport' content='width=device-width, initial-scale=1.0'/> \
-              </head> \
-              <body style='margin: 0; padding: 0;'> \
-               <table align='center' border='0' cellpadding='0' cellspacing='0' width='600' style='border-collapse: collapse;'> \
-                <tr> \
-                 <td> \
-                  <img src='http://www.tilde.net.au/wp-content/uploads/2015/01/header1018-012.jpg' \
-                 </td> \
-                </tr> \
-               </table> \
-              </body> \
-              </html>";
+    message = "";
 
     ss = SpreadsheetApp.getActiveSheet();
     columns = ss.getRange(1, 1, 1, ss.getLastColumn()).getValues()[0];
@@ -69,17 +53,9 @@ function SendConfirmationMail(e) {
     // This is the submitter's email address
     // Make sure you have a field called Email Address in the Google Form
     sender = e.namedValues["Email Address"].toString();
-
-    // Clears out any blank fiels
-    for (var keys in columns) {
-      var key = columns[keys];
-      var val = e.namedValues[key] ? e.namedValues[key].toString() : "";
-      if (val !== "") {
-        message += key + ' :: ' + val + "<br />";
-      }
-    }
-
-    textbody = message.replace("<br>", "\n");
+  
+    // This is just the plain text version that shows up in the
+    textbody = "Hi there, your expression of interest has been received.";
 
     GmailApp.sendEmail(sender, subject, textbody, {
       cc: cc,
